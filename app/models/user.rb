9148455cd10e97bc37cb1ceb5fddf8f72c713694
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-26 16:02:21 -0500

    Add Login and Registration Forms with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :foods, dependent: :destroy
-  has_many :recipes, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  enum role: %i[user moderator admin]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
